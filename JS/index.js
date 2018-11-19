8474f80e55be64582909d760d670fc72326ceba0
jennynzeribe <nzeribejennifer@gmail.com>
2018-11-19 09:12:07 +0100

    my new assignment using arrays and loops

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+//ARRAY CLASS PT1;
+var csdc = {
+    address: "ikeja",
+    value: 200,
+    staff: 20,
+    country: "canada"
+}
+
+console.log(csdc.address);
+var apc = ["tinubu", "buhari", "rochas", "ambode"];
+   apc [0] = "tinubu";
+  console.log(apc[1]);
+  
+  
+  function superJenny (name, school){
+      return " my name is " + name + " i school at " + school;
+
+  }
+  // push() //add new member to end of array
+  // unshift()
+  apc.push("el-rufai");
+  apc.unshift("saraki");
+  console.log(apc);
+  apc[0];
+  console.log(apc[0]);
